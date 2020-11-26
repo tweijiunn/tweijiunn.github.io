5752a89aef411ea28597a183c2b1949ae48d856f
Law Twei Jiunn <tweijiunn@gmail.com>
2020-11-26 11:57:58 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Law Twei Jiunn</h1>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
